55e65b35403bd30b939a99cd7cfe2d594fda5027
Jack Franklin <jack@jackfranklin.net>
2018-08-26 17:39:21 +0100

    configure

---
+++ bin/svg-to-elm.ts
@@ -0,0 +1,65 @@
+import Parser, { ParsingError } from '../src/parser';
+import { ElmModule } from '../src/types';
+
+import yargs from 'yargs';
+import writeElmToDisk from '../src/write-elm-to-disk';
+import chalk from 'chalk';
+
+const argv = yargs
+  .usage('Usage: svg-to-elm <command> [options]')
+  .command('parse', 'Parse an SVG to Elm')
+  .example(
+    '$0 parse --file foo.svg --output Foo.elm --module-name Foo',
+    'Parse foo.svg into an Elm module',
+  )
+  .alias('f', 'file')
+  .alias('o', 'output')
+  .alias('m', 'module-name')
+  .describe('f', 'The SVG to parse')
+  .describe('o', 'The destination to write to')
+  .describe('m', 'The name of the Elm module')
+  .describe(
+    'elm-version',
+    'The version of Elm to use [0.18 or 0.19]. This is passed to elm-format if you use it.',
+  )
+  .describe(
+    'elm-format-path',
+    'The path to Elm format, if you would like the code run through it',
+  )
+  .demandOption(['f'])
+  .demandOption(['o'])
+  .demandOption(['module-name'])
+  .help('h').argv;
+
+const parser = new Parser();
+
+parser
+  .parse(argv.f, {
+    moduleName: argv.m,
+  })
+  .then(result => {
+    if (result.success) {
+      return writeElmToDisk(result as ElmModule, {
+        outputPath: argv.o,
+        elmFormatElmVersion: argv.elmVersion,
+        elmFormatPath: argv.elmFormatPath,
+      });
+    } else {
+      throw (result as ParsingError).message;
+    }
+  })
+  .then(result => {
+    if (result.success) {
+      console.log(
+        chalk.green('Success!'),
+        chalk.blue(argv.f),
+        'parsed and saved as',
+        chalk.blue(argv.o),
+      );
+    } else {
+      throw result;
+    }
+  })
+  .catch(err => {
+    console.log('Error parsing svg:', chalk.red(err.message));
+  });
